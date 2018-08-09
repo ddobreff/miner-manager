89dd1aac4986cf3fff13a8124bb16fac18841d52
Dobromir Dobrev <dobreff@gmail.com>
2018-08-09 17:54:21 +0000

    Adding small monitor script - gpu-tool.sh

---
+++ gpu-tool.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+# Copyright 2018 Dobromir Dobrev <dobreff@gmail.com>
+# This tool provides advanced monitoring for AMD and NVIDIA graphics
+# For simplemining and any other linux OS using ohgodatool
+# This tool is provided without ANY warranty, guarantee and support
+# It falls under the GPL v3.0 license
+# You can use, abuse, modify or whatever you want with it.
+# If you like the tool feel free to donate some beers
+# BTC: 3KpA1zs7W6hd2YYqXFHX5cjyLTi8nBKpZN
+# LTC: M9r2gXp6N1tvbbaKmQ5CVYBEAEzUsx9Nps
+# Enjoy!
+
+DAMN_IGPU=$(lspci | grep -E "Display controller|VGA|3D controller" | head -n 1 | grep -vE "NVIDIA|AMD" |wc -l)
+AMDGPU_COUNT=`/usr/bin/lspci -n -v |egrep -ic "0300: 1002"`
+NVIDIA_COUNT=`/usr/bin/lspci -n -v |egrep -ic "0300: 10de|0302: 10de"`
+TOTAL_COUNT=$( expr $NVIDIA_COUNT + $AMDGPU_COUNT )
+API_PORT=3333
+CLAYMORE_API=$(echo '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}' | nc localhost $API_PORT)
+TOTAL_HASH=$(echo "${CLAYMORE_API}"|jq -r '.result[2] | split(";")[0]|tonumber / 1000')
+
+Printer(){
+        test -t 1 && echo -e "\033[1m $* \033[0m"
+    }
+## Kernel, Uptime, IP Address
+KERNEL_VERSION=`uname -r`
+UPTIME=`uptime -p`
+ipAddress=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+## LOAD DATA TO JSON
+function show_amd_stats() {
+x=0
+while [ $x -lt $1 ]; do
+devid=$x
+if [ $DAMN_IGPU -gt 0 ]; then
+       devid=$((devid+1))
+fi
+        if [ -f /sys/class/drm/card$devid/device/pp_table ]; then
+                GPU_CORE=`cat /sys/class/drm/card$devid/device/pp_dpm_sclk |grep "*" | awk -F  " " '{print $2}' | tr -d 'Mhz' | tr '\n' ' '`
+                GPU_MEMORY=`cat /sys/class/drm/card$devid/device/pp_dpm_mclk |grep "*" | awk -F  " " '{print $2}' | tr -d 'Mhz' | tr '\n' ' '`
+        fi
+
+        if [ -f /sys/class/drm/card$devid/device/hwmon/hwmon?/power1_average ] ; then
+                GPU_POWER=$(( $(echo `cat /sys/class/drm/card$devid/device/hwmon/hwmon?/power1_average`) / 1000000 ))
+		GPU_MAX_POWER=$(( $(echo `cat /sys/class/drm/card$devid/device/hwmon/hwmon?/power1_cap`) / 1000000 ))
+                GPU_TEMP1=$(( $(echo `cat /sys/class/drm/card$devid/device/hwmon/hwmon?/temp1_input`) / 1000 ))
+                GPU_TEMP2=$(( $(echo `cat /sys/class/drm/card$devid/device/hwmon/hwmon?/temp2_input`) / 1000 ))
+        fi
+
+        if [ -f /sys/class/drm/card$devid/device/hwmon/hwmon?/pwm1 ]; then
+                GPU_FANSPEED=$(bc <<< "scale=2; (`cat /sys/class/drm/card$devid/device/hwmon/hwmon?/pwm1`/255)*100" | cut -d \. -f 1)
+        fi
+        if [ -f /sys/kernel/debug/dri/$devid/amdgpu_pm_info ]; then
+                GPU_VOLT=$(cat /sys/kernel/debug/dri/$devid/amdgpu_pm_info |grep 'GPU Voltage' | awk '{print $1}'| sed 's/ //g')
+        fi
+Printer " [[ GPU$devid ==> | CoreClk: ${GPU_CORE}MHz | MemClk: ${GPU_MEMORY}MHz | Power Used: ${GPU_POWER}W | Power CAP: ${GPU_MAX_POWER}W | Voltage: ${GPU_VOLT}mV | Temp: ${GPU_TEMP1}C | ASIC Temp: ${GPU_TEMP2}C | Fanspeed: ${GPU_FANSPEED}% | ]]"
+
+x=$((x+1))
+        TOTAL_GPU_PWR="${TOTAL_GPU_PWR} ${GPU_POWER}"
+done
+        MEM_WATTS=$(($AMDGPU_COUNT * 47))
+        TOTAL_WATTS=$(echo $TOTAL_GPU_PWR $MEM_WATTS 50| xargs  | sed -e 's/\ /+/g' | bc)
+        MINER_EFFICIENCY=$(echo $TOTAL_HASH $TOTAL_WATTS | awk '{print $1 / $2}')
+
+if [ $DAMN_IGPU -gt 0 ] && [ $AMDGPU_COUNT -gt 0 ]; then
+        Printer "\n\t[[ You have enabled iGPU it will be marked as GPU0 and not shown! ]] "
+fi
+
+if [ $AMDGPU_COUNT -gt 0 ]; then
+        Printer "\n\tCurrent hashrate: ${TOTAL_HASH} MH/s"
+        Printer "\tCurrent AMD Total Powerdraw: ${TOTAL_WATTS}W"
+        Printer "\tEfficiency of Claymore Dual ETH Miner: $(printf "%0.2f" ${MINER_EFFICIENCY}) MH per WATT"
+fi
+}
+
+
+function show_nvi_stats() {
+ x=0
+ while [ $x -lt $1 ]; do
+        NVI_GET_INFO=$(/usr/bin/nvidia-smi -i $x --query-gpu=clocks.gr,clocks.mem,power.draw,pstate,fan.speed,temperature.gpu --format=csv,noheader,nounits)
+        GPU_CORE=$(echo $NVI_GET_INFO | awk -F', ' '{print $1}')
+        GPU_MEM=$(echo $NVI_GET_INFO | awk -F', ' '{print $2}')
+        GPU_POWER=$(echo $NVI_GET_INFO | awk -F', ' '{print $3}')
+        GPU_PSTATE=$(echo $NVI_GET_INFO | awk -F', ' '{print $4}')
+        GPU_TEMP=$(echo $NVI_GET_INFO | awk -F', ' '{print $6}')
+        GPU_FAN=$(echo $NVI_GET_INFO | awk -F', ' '{print $5}')
+
+
+Printer " [[ GPU$x ==> | GfxCLK: ${GPU_CORE}MHz | MemClk: ${GPU_MEM}MHz | Power: ${GPU_POWER}W | Power State: ${GPU_PSTATE} | Temp: ${GPU_TEMP}C | Fanspeed: ${GPU_FAN}% | ]]"
+x=$((x+1))
+        TOTAL_GPU_PWR="${TOTAL_GPU_PWR} ${GPU_POWER}"
+done
+        TOTAL_WATTS=$(echo $TOTAL_GPU_PWR 50| xargs  | sed -e 's/\ /+/g' | bc)
+        MINER_EFFICIENCY=$(echo $TOTAL_HASH $TOTAL_WATTS | awk '{print $1 / $2}')
+if [ $NVIDIA_COUNT -gt 0 ]; then
+        Printer "\n\tCurrent hashrate: ${TOTAL_HASH} MH/s"
+        Printer "\tCurrent NVIDIA Total Powerdraw: ${TOTAL_WATTS}W"
+        Printer "\tEfficiency of Claymore Dual ETH Miner: $(printf "%0.2f" ${MINER_EFFICIENCY}) MH per WATT"
+fi
+}
+
+function gpu_detect_amd() {
+devid=$1
+
+if [ $DAMN_IGPU -gt 0 ]; then
+       devid=$((devid+1))
+fi
+
+if [ $DAMN_IGPU -gt 0 ] && [ $AMDGPU_COUNT -gt 0 ]; then
+        Printer "\n[[ You have enabled iGPU it will be marked as GPU0 and not taken in count! ]] "
+fi
+
+if [ $AMDGPU_COUNT -gt 0 ]; then
+        #killing miner
+        screen -S miner -X quit
+	Printer "\n[[ Found $AMDGPU_COUNT AMD GPUs in the system ]] "
+	Printer "\n[[ Setting fan to 0% for GPU: $devid ]] "
+        Printer "\n[[ Command will repeat 10 times to make sure fan is totally stopped! ]] "
+        Printer "\n[[ Make sure to remain eye contact on the Rig while spinning down fans! ]] "
+	for ((n=0;n<10;n++))
+	do
+# Uncomment next line if you use simplemining
+        /root/utils/wolfamdctrl -i $devid --set-fanspeed 0 > /dev/null 2>&1
+# Uncomment next line if you use mmpOS
+#        /opt/mmp/bin/ohgodatool -i $devid --set-fanspeed 0 > /dev/null 2>&1
+	sleep 0.5
+	done
+else
+	Printer "No AMD Hardware found!"
+	exit 0
+fi
+}
+
+function gpu_detect_nvidia() {
+devid=$1
+if [ $NVIDIA_COUNT -gt 0 ]; then
+        #killing miner
+	screen -S miner -X quit
+        Printer "\n[[ Found $NVIDIA_COUNT AMD GPUs in the system ]] "
+        Printer "\n[[ Setting fan to 0% on GPU: $devid ]] "
+        Printer "\n[[ Command will repeat 2 times to make sure fan is totally stopped! ]] "
+        Printer "\n[[ Make sure to remain eye contact on the Rig while spinning down fans! ]] "
+        for ((n=0;n<2;n++))
+        do
+		DISPLAY=:0.$devid /usr/bin/nvidia-settings -a [gpu:$devid]/GPUFanControlState=1 -a [fan:$devid]/GPUTargetFanSpeed=0
+		sleep 0.2
+        done
+else
+        echo -e "No NVIDIA Hardware found!"
+        exit 0
+fi
+}
+
+function show_help() {
+        echo -e "Usage: '--efficiency','--detect *gpunum*'"
+}
+
+case "$1" in
+	--efficiency)
+show_amd_stats $AMDGPU_COUNT
+show_nvi_stats $NVIDIA_COUNT
+Printer "\n\tShowing total of $TOTAL_COUNT GPU information"
+Printer "\tOf which $AMDGPU_COUNT are AMD and $NVIDIA_COUNT are NVIDIA"
+Printer "\n\tRig Kernel: $KERNEL_VERSION,\t Rig Uptime: $UPTIME,\t IP:  $ipAddress"
+;;
+        --detect)
+gpu_detect_amd
+gpu_detect_nvidia
+;;
+        --help|-h|*)
+        show_help
+;;
+esac
+exit 0
